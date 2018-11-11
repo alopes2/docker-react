b1399fe2dfb914e4d7d600005acf8fe86a938fc7
andre <andrevitorlopes@gmail.com>
2018-11-11 20:24:21 -0200

    changing to test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hellor There!
           </a>
         </header>
       </div>
